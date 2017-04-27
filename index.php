1a91a8699468f21ab4fa2ae0b27e3b0efbe54510
Pavel Shabashov <forcu2mblog@gmail.com>
2017-04-27 23:37:44 +0400

    module1-task2: make sure, that code is okay with standards

--- index.php
+++ index.php
@@ -28,14 +28,14 @@ $user_avatar = 'img/user.jpg';
 
         <nav class="user-menu">
 
-            <?php if ($is_auth): ?>
+            <?php if ($is_auth) : ?>
                 <div class="user-menu__image">
                     <img src="<?= $user_avatar ?>" width="40" height="40" alt="Пользователь">
                 </div>
                 <div class="user-menu__logged">
                     <p><?= htmlspecialchars($user_name) ?></p>
                 </div>
-            <?php else: ?>
+            <?php else : ?>
                 <ul class="user-menu__list">
                     <li class="user-menu__item">
                       <a href="#">Регистрация</a>
